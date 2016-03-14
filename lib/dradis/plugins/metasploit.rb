528cd0eb6b05a7c238fc407ed6933cfa100c2b0b
Daniel Martin <etd-github@nomejortu.com>
2016-03-14 12:46:30 +0100

    We no longer need a Meta module

--- lib/dradis/plugins/metasploit.rb
+++ lib/dradis/plugins/metasploit.rb
@@ -9,20 +9,3 @@ require 'dradis/plugins/metasploit/engine'
 require 'dradis/plugins/metasploit/field_processor'
 require 'dradis/plugins/metasploit/importer'
 require 'dradis/plugins/metasploit/version'
-
-# This is required while we transition the Upload Manager to use
-# Dradis::Plugins only
-module Dradis
-  module Plugins
-    module Metasploit
-      module Meta
-        NAME = "Metasploit Framework database dump (XML)"
-        EXPECTS = "Expects Metasploit XML, use: db_export"
-
-        module VERSION
-          include Dradis::Plugins::Metasploit::VERSION
-        end
-      end
-    end
-  end
-end
