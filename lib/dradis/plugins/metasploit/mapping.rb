286f8a81326fa4deb69828cebe9a1306d90e2ed7
Matt Budz <matt.budz@protonmail.com>
2024-03-07 17:29:13 +0100

    add default mapping

--- lib/dradis/plugins/metasploit/mapping.rb
+++ lib/dradis/plugins/metasploit/mapping.rb
@@ -1,6 +1,13 @@
 module Dradis::Plugins::Metasploit
   module Mapping
     def self.default_mapping
+      {
+        'host_note' => {
+          'Title' => 'Note {{ metasploit[host_note.id] }}',
+          'Type' => '{{ metasploit[host_note.ntype] }}',
+          'Data' => '{{ metasploit[host_note.data] }}'
+        }
+      }
     end
   end
 end
